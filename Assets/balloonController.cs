246f7f8f4ace70fc7463311b3bf8f0d1c80d56f7
Konrad9 <konrad9@gmail.com>
2014-07-19 11:52:28 -0700

    7-19-2014
    added sidewall bounce

---
+++ Assets/balloonController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class balloonController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collider)
+	{
+		Debug.Log (collider.gameObject.tag);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
