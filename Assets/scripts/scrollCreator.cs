c1f2722ae5ded38b62ecd21399e7145d963e94a4
Sean <konrad9@gmail.com>
2014-08-11 22:49:00 -0700

    scrolling background take 2

---
+++ Assets/scripts/scrollCreator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrollCreator : MonoBehaviour {
+
+	public GameObject scroller;
+	private Vector3 newScrollerPosition;
+
+	// Use this for initialization
+	void Start () {
+		newScrollerPosition = new Vector3(0f, 12.5f, 0f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter()
+	{
+		Instantiate(scroller, newScrollerPosition, Quaternion.identity);
+		
+	}
+}
