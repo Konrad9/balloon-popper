3c307d5022d713ce3f1ecbe714fe1db523069503
Konrad9 <konrad9@gmail.com>
2014-09-28 22:09:41 -0700

    battleship prefab, update to battleship and missile

--- Assets/scripts/enemyBattleshipController.cs
+++ Assets/scripts/enemyBattleshipController.cs
@@ -6,7 +6,10 @@ public class enemyBattleshipController : MonoBehaviour {
 	public GameObject laserBeam; 
 
 
+	private Vector3 velocity; 
+
 	private float xVelocity;
+	private float yVelocity; 
 	private float missileReloadTimer;
 	private float missileReloadWait;
 	private float laserReloadTimer;
@@ -20,14 +23,28 @@ public class enemyBattleshipController : MonoBehaviour {
 	void Start () {
 		///slightly more than 1/2 basic enemy speed
 		xVelocity = 2.5f; 
+		yVelocity = 1.5f; 
 		laserReloadWait = 1.5f;
 		missileReloadWait = 5.0f;
 		hp = 100f;
 		shieldHP = 50f;
+		velocity = new Vector3(xVelocity, yVelocity, 0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		gameObject.rigidbody.velocity = velocity; 
+
+		if (gameObject.transform.position.y < 9)
+		{
+			velocity.y = yVelocity; 
+		}
+
+		if (gameObject.transform.position.y > 16)
+		{
+			velocity.y = - yVelocity; 
+		}
 
 		if ((missileReloadTimer += Time.deltaTime) >= missileReloadWait)
 		{
@@ -38,14 +55,14 @@ public class enemyBattleshipController : MonoBehaviour {
 		{
 			fireLaser();
 		}
-	
 	}
 
 	private void OnTriggerEnter(Collider collider)
 	{
 		if (collider.tag == "wall")
 		{
-			xVelocity = xVelocity * -1; 
+			velocity.x *= -1; 
+			Debug.Log ("hitwall");
 			gameObject.rigidbody.velocity = new Vector3(xVelocity, gameObject.rigidbody.velocity.y, 0.0f);
 		}
 	}
---
+++ Assets/scripts/enemyMissileController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMissileController : MonoBehaviour {
+	private float yVelocity; 
+	public GameObject player; 
+	private Vector3 fireTrajectory; 
+	private Vector3 velocity; 
+
+	// Use this for initialization
+	void Start () {
+		velocity = new Vector3(0, 4, 0); 
+		player = GameObject.Find ("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		///compute basic trajectory to player, go there, blow up if hit by bullet
+
+		fireTrajectory = (player.transform.position - gameObject.transform.position).normalized;
+		gameObject.transform.LookAt(player.transform);
+		gameObject.rigidbody.position += fireTrajectory * 4.0f * Time.deltaTime; 
+	}
+}
+/*
+
+void Start()
+{
+	movementDirection = (pathPoints[currentPathIndex].transform.position - transform.position).normalized;
+}
+
+// Update is called once per frame
+void Update ()
+{	
+	//movement code
+	transform.position += movementDirection*speed*Time.deltaTime;
+}*/
